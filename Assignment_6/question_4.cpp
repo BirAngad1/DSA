e99ca5c86c5af85d415bac43a6d96c25c7f3d9a9
BirAngad1 <birangad25@gmail.com>
2025-11-06 15:08:55 +0530

    Update question_4.cpp

--- Assignment_6/question_4.cpp
+++ Assignment_6/question_4.cpp
@@ -3,24 +3,25 @@ using namespace std;
 
 class D {
 public:
-    char v;
-    D *pr, *nx;
+    char v; D *pr, *nx;
     D(char x) : v(x), pr(NULL), nx(NULL) {}
 };
 
 bool isPal(D *h) {
     if (!h) return 1;
-    D *l = h;
-    D *r = h;
+    D *l = h, *r = h;
     while (r->nx) r = r->nx;
     while (l != r && l->pr != r) {
         if (l->v != r->v) return 0;
-        l = l->nx;
-        r = r->pr;
+        l = l->nx; r = r->pr;
     }
     return 1;
 }
 
 int main() {
+    string s; cin >> s;
+    D *h = NULL, *t = NULL;
+    for (char c : s) { D *p = new D(c); if (!h) { h = t = p; } else { t->nx = p; p->pr = t; t = p; } }
+    cout << (isPal(h) ? "True\n" : "False\n");
     return 0;
 }
