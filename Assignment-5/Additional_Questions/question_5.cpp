3661edbb8ba6f8f34b95ab0ff15fcf991993c628
BirAngad1 <birangad25@gmail.com>
2025-11-06 15:12:48 +0530

    Update question_5.cpp

--- Assignment-5/Additional_Questions/question_5.cpp
+++ Assignment-5/Additional_Questions/question_5.cpp
@@ -1,26 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Poly{int c,p;Poly*next;Poly(int C,int P):c(C),p(P),next(NULL){}};
+class P {
+public:
+    int c, p;
+    P *next;
+    P(int C, int Pwr) : c(C), p(Pwr), next(NULL) {}
+};
 
-Poly* add(Poly*a,Poly*b){
-    Poly d(0,0); Poly* t=&d;
-    while(a&&b){
-        if(a->p==b->p){
-            int s=a->c+b->c;
-            if(s) t->next=new Poly(s,a->p), t=t->next;
-            a=a->next; b=b->next;
-        }else if(a->p>b->p){
-            t->next=new Poly(a->c,a->p); t=t->next; a=a->next;
-        }else{
-            t->next=new Poly(b->c,b->p); t=t->next; b=b->next;
+class Poly {
+public:
+    P *h = NULL;
+
+    void push_back(int c, int p) {
+        P *n = new P(c, p);
+        if (!h) { h = n; return; }
+        P *t = h;
+        while (t->next) t = t->next;
+        t->next = n;
+    }
+
+    void print_pairs() {
+        P *t = h;
+        while (t) {
+            cout << "[" << t->c << "," << t->p << "]";
+            if (t->next) cout << " ";
+            t = t->next;
+        }
+        cout << "\n";
+    }
+};
+
+P* add(P *a, P *b) {
+    P d(0,0);
+    P *t = &d;
+    while (a && b) {
+        if (a->p == b->p) {
+            int s = a->c + b->c;
+            if (s) { t->next = new P(s, a->p); t = t->next; }
+            a = a->next; b = b->next;
+        } else if (a->p > b->p) {
+            t->next = new P(a->c, a->p); t = t->next; a = a->next;
+        } else {
+            t->next = new P(b->c, b->p); t = t->next; b = b->next;
         }
     }
-    while(a){ t->next=new Poly(a->c,a->p); t=t->next; a=a->next; }
-    while(b){ t->next=new Poly(b->c,b->p); t=t->next; b=b->next; }
+    while (a) { t->next = new P(a->c, a->p); t = t->next; a = a->next; }
+    while (b) { t->next = new P(b->c, b->p); t = t->next; b = b->next; }
     return d.next;
 }
 
-int main(){
+int main() {
+    int n, m;
+    cin >> n;
+    Poly A, B, C;
+    for (int i = 0; i < n; i++) { int c, p; cin >> c >> p; A.push_back(c, p); }
+    cin >> m;
+    for (int i = 0; i < m; i++) { int c, p; cin >> c >> p; B.push_back(c, p); }
+    C.h = add(A.h, B.h);
+    C.print_pairs();
     return 0;
 }
