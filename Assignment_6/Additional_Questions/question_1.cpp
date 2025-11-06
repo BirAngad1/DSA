f2aa2d1af33eabd93a1de3aee3a9fdd18bb9210f
BirAngad1 <birangad25@gmail.com>
2025-11-06 15:09:26 +0530

    Update question_1.cpp

--- Assignment_6/Additional_Questions/question_1.cpp
+++ Assignment_6/Additional_Questions/question_1.cpp
@@ -3,25 +3,27 @@ using namespace std;
 
 class N {
 public:
-    int v;
-    N *nx;
+    int v; N *nx;
     N(int x) : v(x), nx(NULL) {}
 };
 
 void split(N *h, N *&a, N *&b) {
     if (!h) { a = NULL; b = NULL; return; }
-    N *slow = h, *fast = h;
-    while (fast->nx != h && fast->nx->nx != h) {
-        slow = slow->nx;
-        fast = fast->nx->nx;
-    }
-    if (fast->nx->nx == h) fast = fast->nx;
-    a = h;
-    b = slow->nx;
-    fast->nx = b;
-    slow->nx = a;
+    N *s = h, *f = h;
+    while (f->nx != h && f->nx->nx != h) { s = s->nx; f = f->nx->nx; }
+    if (f->nx->nx == h) f = f->nx;
+    a = h; b = s->nx;
+    f->nx = b; s->nx = a;
 }
 
+void print(N *h) { if (!h) { cout << "\n"; return; } N *t = h; do { cout << t->v << " "; t = t->nx; } while (t != h); cout << "\n"; }
+
 int main() {
+    int n; cin >> n;
+    N *h = NULL, *t = NULL;
+    while (n--) { int x; cin >> x; N *p = new N(x); if (!h) { h = t = p; } else { t->nx = p; t = p; } }
+    if (h) t->nx = h;
+    N *a, *b; split(h, a, b);
+    print(a); print(b);
     return 0;
 }
