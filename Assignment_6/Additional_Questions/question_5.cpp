f0be6ce5ac29187dedc7d909be3c891e604e6e4c
BirAngad1 <birangad25@gmail.com>
2025-11-06 15:10:26 +0530

    Update question_5.cpp

--- Assignment_6/Additional_Questions/question_5.cpp
+++ Assignment_6/Additional_Questions/question_5.cpp
@@ -3,19 +3,15 @@ using namespace std;
 
 class X {
 public:
-    int v;
-    X *l, *r, *u, *d;
+    int v; X *l, *r, *u, *d;
     X(int x) : v(x), l(NULL), r(NULL), u(NULL), d(NULL) {}
 };
 
 vector<vector<X*>> build(const vector<vector<int>> &a) {
-    int n = a.size();
-    if (!n) return {};
+    int n = a.size(); if (!n) return {};
     int m = a[0].size();
     vector<vector<X*>> g(n, vector<X*>(m, NULL));
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            g[i][j] = new X(a[i][j]);
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) g[i][j] = new X(a[i][j]);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (j + 1 < m) { g[i][j]->r = g[i][j + 1]; g[i][j + 1]->l = g[i][j]; }
@@ -26,5 +22,15 @@ vector<vector<X*>> build(const vector<vector<int>> &a) {
 }
 
 int main() {
+    int n, m; cin >> n >> m;
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) cin >> a[i][j];
+    auto g = build(a);
+    if (n && m) {
+        cout << g[0][0]->v << " ";
+        if (m > 1) cout << g[0][0]->r->v << " ";
+        if (n > 1) cout << g[0][0]->d->v << "\n";
+        else cout << "\n";
+    } else cout << "\n";
     return 0;
 }
